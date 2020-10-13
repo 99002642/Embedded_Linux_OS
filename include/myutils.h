1efa7e456fe579b4d66d370f49b3607ecb9d6a0b
99002642 <71489688+99002642@users.noreply.github.com>
2020-10-13 13:07:49 +0530

    Added Header Files via upload

---
+++ include/myutils.h
@@ -0,0 +1,16 @@
+  
+#ifndef __MYUTILS_H__
+#define __MYUTILS_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdarg.h>
+
+long factorial(int number);
+int isPalindrome(int number);
+int isPrime(int n);
+int vsum(int numero,...);
+
+#endif
+
